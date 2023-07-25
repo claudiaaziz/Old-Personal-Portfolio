d94eeb427e6373cd852e0241afe6c643f8598f41
Claudia Aziz <claudiahannahh@gmail.com>
2023-07-25 10:41:03 -0400

    changed placeholder imgs & changed css layout of the sections to be 100vh

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const imgs = [
-  "https://i.pinimg.com/originals/25/c4/ef/25c4efbf918a698f50e05f545050491c.jpg",
-  "https://offactandfantasy.files.wordpress.com/2020/05/magic-of-pegasus-18.png?w=1024",
-  "https://offactandfantasy.files.wordpress.com/2020/05/magic-of-pegasus-22.png?w=1024",
-  "https://offactandfantasy.files.wordpress.com/2020/05/magic-of-pegasus-16.png?w=1024",
+  "/images/pexels-kevin-villaruz-7252278.jpg",
+  "/images/pexels-the-lazy-artist-gallery-1302991.jpg",
+  "/images/pexels-abid-bin-nazar-10593605.jpg",
+  "/images/pexels-abid-bin-nazar-10593631.jpg",
 ]
 
 const btn = document.querySelector("button")
 const img = document.querySelector(".main-img")
 let count = 0
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", () => {
   if (count === 4) count = 0
   img.src = imgs[count]
   count++
