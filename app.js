07e24c0f22fc8298d60835b271e86c19b0ad0212
claudiaaziz <claudiahannahh@gmail.com>
2023-08-18 07:59:28 -0400

    removed 1 "project"

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const imgs = [
   "/images/pexels-kevin-villaruz-7252278.jpg",
-  "/images/pexels-the-lazy-artist-gallery-1302991.jpg",
   "/images/pexels-abid-bin-nazar-10593605.jpg",
   "/images/pexels-abid-bin-nazar-10593631.jpg",
 ]
@@ -10,7 +9,7 @@ const img = document.querySelector(".main-img")
 let count = 0
 
 btn.addEventListener("click", () => {
-  if (count === 4) count = 0
+  if (count === 3) count = 0
   img.src = imgs[count]
   count++
 })
